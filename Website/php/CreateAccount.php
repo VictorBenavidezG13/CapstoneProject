d8b11cbda4e3ce8534d446bc6cbf43a5500c3481
kjcamiliere <kcamilie@asu.edu>
2017-01-21 14:32:48 -0700

    User Table now works in mysql.

--- Website/php/CreateAccount.php
+++ Website/php/CreateAccount.php
@@ -1,5 +1,6 @@
 <?php
 // script for having a user create an account
+// fires when the user clicks 'submit'
 
 // Blank Variables 
 $account_emailAddress = "";
@@ -8,14 +9,14 @@ $account_password = "";
 
 // Server Variables
 $server_username = "root";
-$server_password = "password";
+$server_password = "";
 $server_server = "127.0.0.1";
 $server_database = "mydb";
 
 
 // Checks for dangerous characters and sets the values for the variables
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $account_emailAddress = validateInput($_POST["emailAddress"]);
+    $account_emailAddress = validateInput($_POST["email"]);
     $account_username = validateInput($_POST["username"]);
     $account_password = validateInput($_POST["password"]);
 
@@ -32,11 +33,15 @@ if ($conn->connect_error) {
 echo "Connected successfully";
 
 // Add the entry into the table
-"INSERT INTO users(email, username, password)
-VALUES ('$account_emailAddress','$account_username', '$account_password')";
+$sql = "INSERT INTO users(email, username, password)
+VALUES ($account_emailAddress,$account_username, $account_password)";
 
 
-
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
 
 
 
